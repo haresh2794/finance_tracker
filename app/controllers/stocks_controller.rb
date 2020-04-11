8a0b23bb2447ce3e98d7741236ea7fadedc949f4
Haresh <hareshmvc@gmail.com>
2020-04-11 22:54:38 +0530

    Chap 251 flash message using Ajax

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,14 +4,21 @@ def search
   if params[:stock].present?
     @stock = Stock.new_lookup(params[:stock])
     if @stock
-      render 'users/my_portfolio'
+      respond_to do |format|
+        format.js { render partial: 'users/result' }
+      end
+       
     else
-      flash[:alert] = "Please enter a valid symbol to search"
-      redirect_to my_portfolio_path
-    end
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a valid symbol to search"
+        format.js { render partial: 'users/result' }
+      end
+    end   
   else
-    flash[:alert] = "Please Enter a Symbol"
-    redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result' }
+    end
   end
 end
 
