f8f8608b8632dc3754a4307d6934d2d87a09784d
Haresh <hareshmvc@gmail.com>
2020-04-11 18:40:54 +0530

    Chap 248 Invalid stock symbol warnings

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,18 @@
 class StocksController < ApplicationController
 
 def search
- 
+  if params[:stock].present?
     @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
-
- 
+    if @stock
+      render 'users/my_portfolio'
+    else
+      flash[:alert] = "Please enter a valid symbol to search"
+      redirect_to my_portfolio_path
+    end
+  else
+    flash[:alert] = "Please Enter a Symbol"
+    redirect_to my_portfolio_path
+  end
 end
 
 end
