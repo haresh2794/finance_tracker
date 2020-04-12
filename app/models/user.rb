c306256fed770253cbc7555d73658d7bed1c4ab8
Haresh <hareshmvc@gmail.com>
2020-04-12 19:42:18 +0530

    Chap 259 User model with first & last name and nav bar view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
+
 has_many :user_stocks
 has_many :stocks, through: :user_stocks
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -19,8 +19,11 @@ has_many :stocks, through: :user_stocks
 
   def can_track_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
-    
   end
 
-  
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonymous"
+  end
+
 end
